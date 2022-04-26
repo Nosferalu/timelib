674aa19c6c55503cc2b07e2c7ac548eea40db75f
Alexander Miller <a.miller@basecom.de>
2022-04-26 12:49:19 +0200

    new Library for date functions

---
+++ Header Files/zeit.h
@@ -0,0 +1,10 @@
+#ifndef ZEIT_H_INCLUDED
+#define ZEIT_H_INCLUDED
+
+int is_leapyear(int year);
+int day_of_the_year(int day, int month, int year);
+void input_date(int *day, int *month, int *year);
+int exists_date(int day, int month, int year); 
+int get_days_for_month(int month, int year); 
+
+#endif
